e38339e95caac05086e80b6ab5bd3f1688778152
Myki487 <154702077+Myki487@users.noreply.github.com>
2025-12-08 16:21:47 +0200

    BasicAuthorization
    Added a simple page and logic for:
    - registration
    - login
    - personal account

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,9 @@
+import LoginForm from '@/src/components/auth/LoginForm'
+
+export default function LoginPage() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <LoginForm />
+    </div>
+  )
+}
